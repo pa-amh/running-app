1f8ca98432a17fefa1195c67f73b755815b1fee6
physicsboy <amhighton@gmail.com>
2020-06-12 21:23:45 +0100

    Add / response in server

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const port = 3030;
 express()
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({extended: true}))
+    .get('/', function (req, res) {
+        res.json(200, {msg: 'hello world'});
+    })
     .get('/api', function (req, res) {
         res.json(200, {msg: 'OK' });
     })
