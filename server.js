976fdcf8c9ab62b64b5b0c7f9baaaad819dedda1
physicsboy <amhighton@gmail.com>
2020-06-13 17:04:51 +0100

    server test

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ const PORT = process.env.PORT || 8080;
 const PG_CONNECTION_STRING = process.env.DATABASE_URL || 'postgresql://postgres:Adzisace123@localhost:5432/react-running-app';
 const pool = new Pool({
     connectionString: PG_CONNECTION_STRING,
-    ssl: !!process.env.DATABASE_URL
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 express()
     .use(express.static(__dirname))
     .use(express.static(path.join(__dirname, 'build')))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .get('/ping', (req, res) => res.send('blob'))
+    .get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    })
+    .get('/ping', function (req, res) {
+        return res.send('pong');
+    })
     .get('/db', async (req, res) => {
         try {
             const client = await pool.connect();
@@ -27,27 +32,4 @@ express()
             res.send("Error " + err);
         }
     })
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-// express()
-//     .use(express.static(__dirname))
-//     .use(express.static(path.join(__dirname, 'build')))
-//     .get('/', function (req, res) {
-//         res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//     })
-//     .get('/ping', function (req, res) {
-//         return res.send('pong');
-//     })
-//     .get('/db', async (req, res) => {
-//         try {
-//             const client = await pool.connect();
-//             const result = await client.query('SELECT * FROM test_table');
-//             const results = { 'results': (result) ? result.rows : null};
-//             res.render('pages/db', results );
-//             client.release();
-//         } catch (err) {
-//             console.error(err);
-//             res.send("Error " + err);
-//         }
-//     })
-//     .listen(port, () => console.log(`Listening on port: ${port}`));
+    .listen(PORT, () => console.log(`Listening on port: ${PORT}`));
